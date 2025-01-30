abcb5f36c889790ee016e030ebea164b45e12d6c
Jose Martinez <jm2233521@gmail.com>
2025-01-30 14:27:32 -0600

    Primera version

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let nombreObtenido = document.getElementById('amigo').value;
+    console.log(nombreObtenido);  
+    if (nombreObtenido !== '') {
+        amigos.push(nombreObtenido);
+        document.querySelector('#amigo').value = '';   
+        console.log(amigos);  
+        return nombreObtenido;                  
+    } else {        
+        alert('Por favor, inserte un nombre');      
+    }         
+    return;        
+}
+document.getElementById('agregar').onclick = agregarAmigo;
